6dbbc0160555bc959317cf5ef9aec7da24ce3c1f
RomanDanielGutierrezz <romandanielgutierrez@gmail.com>
2025-05-16 13:17:14 +0000

    subiendo desde git

---
+++ Guia7.4/Ejercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantZonas = 0;
+
+            Console.WriteLine("Solicitar Cantidad de zonas");
+            cantZonas = Convert.ToInt32(Console.ReadLine());
+
+            for (int zonas = 1; zonas <= cantZonas; zonas++)
+            {
+                
+            }
+        }
+    }
+}
